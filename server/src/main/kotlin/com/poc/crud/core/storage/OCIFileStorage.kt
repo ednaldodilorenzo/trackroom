5a69e90f926f384bb70935501b9c997fdb18cc0b
edsf <ednaldo.dilorenzo@gmail.com>
2026-01-23 11:31:14 -0300

    Changes to deploy in oracle oci.

--- server/src/main/kotlin/com/poc/crud/core/storage/OCIFileStorage.kt
+++ server/src/main/kotlin/com/poc/crud/core/storage/OCIFileStorage.kt
@@ -1,23 +1,25 @@
 package com.poc.crud.core.storage
 
-import com.oracle.bmc.objectstorage.ObjectStorageClient
+import com.oracle.bmc.objectstorage.ObjectStorage
 import com.oracle.bmc.objectstorage.model.CreatePreauthenticatedRequestDetails
 import com.oracle.bmc.objectstorage.requests.CreatePreauthenticatedRequestRequest
 import com.oracle.bmc.objectstorage.requests.GetObjectRequest
 import com.oracle.bmc.objectstorage.requests.PutObjectRequest
-import jdk.jfr.ContentType
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.context.annotation.Profile
 import org.springframework.stereotype.Component
-import org.springframework.web.multipart.MultipartFile
 import java.io.InputStream
 import java.time.Duration
 import java.time.Instant
-import java.util.Date
+import java.util.*
 
 
 @Component
 @Profile("oci")
-class OCIFileStorage(private val objectStorageClient: ObjectStorageClient) : FileStorage {
+class OCIFileStorage(
+    private val objectStorageClient: ObjectStorage,
+    @Value("\${filestorage.endpoint}") private val ociStorageUrl: String
+) : FileStorage {
     override fun saveFile(
         bucketName: String,
         contentType: String,
@@ -26,72 +28,45 @@ class OCIFileStorage(private val objectStorageClient: ObjectStorageClient) : Fil
         fileSize: Long,
     ) {
         val request =
-            PutObjectRequest.builder()
-                .namespaceName("namespace")
-                .bucketName(bucketName)
-                .objectName(fileName)
-                .putObjectBody(file)
-                .contentLength(fileSize) // IMPORTANT
-                .contentType("application/octet-stream")
-                .build()
+            PutObjectRequest.builder().namespaceName("gryq8b2i1p2v").bucketName(bucketName).objectName(fileName)
+                .putObjectBody(file).contentLength(fileSize) // IMPORTANT
+                .contentType("application/octet-stream").build()
 
         objectStorageClient.putObject(request)
     }
 
     override fun getFile(bucketName: String, fileName: String): InputStream {
-        val request = GetObjectRequest.builder()
-            .namespaceName(bucketName)
-            .bucketName(bucketName)
-            .objectName(fileName)
-            .build()
+        val request =
+            GetObjectRequest.builder().namespaceName("gryq8b2i1p2v").bucketName(bucketName).objectName(fileName).build()
         return objectStorageClient.getObject(request).inputStream
     }
 
     override fun getFileUrl(bucketName: String, fileName: String): String {
         return createPar(
-            bucketName,
-            fileName,
-            CreatePreauthenticatedRequestDetails.AccessType.ObjectRead,
-            Duration.ofMinutes(2)
-        );
+            bucketName, fileName, CreatePreauthenticatedRequestDetails.AccessType.ObjectRead, Duration.ofMinutes(2)
+        )
     }
 
     override fun getUploadUrl(
-        bucketName: String,
-        fileName: String,
-        contentType: String
+        bucketName: String, fileName: String, contentType: String
     ): String {
         return createPar(
-            bucketName,
-            fileName,
-            CreatePreauthenticatedRequestDetails.AccessType.ObjectWrite,
-            Duration.ofMinutes(2)
-        );
+            bucketName, fileName, CreatePreauthenticatedRequestDetails.AccessType.ObjectWrite, Duration.ofMinutes(2)
+        )
     }
 
     private fun createPar(
-        bucket: String?,
-        key: String?,
-        accessType: CreatePreauthenticatedRequestDetails.AccessType?,
-        ttl: Duration?
+        bucket: String?, key: String?, accessType: CreatePreauthenticatedRequestDetails.AccessType?, ttl: Duration?
     ): String {
         val details =
-            CreatePreauthenticatedRequestDetails.builder()
-                .name("par-" + key + "-" + Instant.now().toEpochMilli())
-                .accessType(accessType)
-                .objectName(key)
-                .timeExpires(Instant.now().plus(ttl) as Date?)
-                .build()
+            CreatePreauthenticatedRequestDetails.builder().name("par-" + key + "-" + Instant.now().toEpochMilli())
+                .accessType(accessType).objectName(key).timeExpires(Date.from(Instant.now().plus(ttl))).build()
 
-        val response =
-            objectStorageClient.createPreauthenticatedRequest(
-                CreatePreauthenticatedRequestRequest.builder()
-                    .namespaceName("namespace")
-                    .bucketName(bucket)
-                    .createPreauthenticatedRequestDetails(details)
-                    .build()
-            )
+        val response = objectStorageClient.createPreauthenticatedRequest(
+            CreatePreauthenticatedRequestRequest.builder().namespaceName("gryq8b2i1p2v").bucketName(bucket)
+                .createPreauthenticatedRequestDetails(details).build()
+        )
 
-        return response.preauthenticatedRequest.accessUri.toString()
+        return ociStorageUrl + response.preauthenticatedRequest.accessUri.toString()
     }
 }
