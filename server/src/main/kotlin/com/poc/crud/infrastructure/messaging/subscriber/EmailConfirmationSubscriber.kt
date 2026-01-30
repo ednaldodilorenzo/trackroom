4a1c3561f60aa5348f152b06d8411558c644886b
edsf <ednaldo.dilorenzo@gmail.com>
2026-01-30 14:02:01 -0300

    [FIX] Add variable to front url in back end.

--- server/src/main/kotlin/com/poc/crud/infrastructure/messaging/subscriber/EmailConfirmationSubscriber.kt
+++ server/src/main/kotlin/com/poc/crud/infrastructure/messaging/subscriber/EmailConfirmationSubscriber.kt
@@ -6,15 +6,24 @@ import com.poc.crud.core.queue.Task
 import com.poc.crud.core.queue.TaskType
 import com.poc.crud.infrastructure.email.EmailSenderService
 import com.poc.crud.infrastructure.messaging.task.AccountConfirmationTaskPayload
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.stereotype.Component
 
 @Component
-class EmailConfirmationSubscriber(private val objectMapper: ObjectMapper, private val mailService: EmailSenderService) : MessageSubscriber {
+class EmailConfirmationSubscriber(
+    private val objectMapper: ObjectMapper,
+    private val mailService: EmailSenderService,
+    @Value("\${APP_FRONT_URL}") private val frontURL: String
+) : MessageSubscriber {
     override val supports: TaskType = TaskType.MESSAGE_EMAIL_CONFIRMATION
     override fun processMessage(task: Task) {
         val emailPayload: AccountConfirmationTaskPayload =
             objectMapper.treeToValue(task.payload, AccountConfirmationTaskPayload::class.java)
-        mailService.sendEmail(emailPayload.email, "Código de validação de email", """Link: http://localhost/confirm/${emailPayload.token} <br> ${emailPayload.code}""")
+        mailService.sendEmail(
+            emailPayload.email,
+            "Código de validação de email",
+            """Link: ${frontURL}/confirm/${emailPayload.token} <br> ${emailPayload.code}"""
+        )
         println("Send email ${emailPayload.email} code ${emailPayload.code}")
     }
 }
