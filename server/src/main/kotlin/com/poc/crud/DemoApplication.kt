90d5b551145efff41c077f24502fc3a282ab1b1e
edsf <ednaldo.dilorenzo@gmail.com>
2026-02-03 20:15:23 -0300

    Add oci message queues.

--- server/src/main/kotlin/com/poc/crud/DemoApplication.kt
+++ server/src/main/kotlin/com/poc/crud/DemoApplication.kt
@@ -2,9 +2,11 @@ package com.poc.crud
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.scheduling.annotation.EnableScheduling
 
 
 @SpringBootApplication
+@EnableScheduling
 class DemoApplication
 
 fun main(args: Array<String>) {
