4eeedcac7050cde69113b2314df63692b1fd048e
edsf <ednaldo.dilorenzo@gmail.com>
2026-02-03 18:05:48 -0300

    Add oci message queues.

--- server/src/main/kotlin/com/poc/crud/config/queue/OciQueueConfig.kt
+++ server/src/main/kotlin/com/poc/crud/config/queue/OciQueueConfig.kt
@@ -3,6 +3,7 @@ package com.poc.crud.config.queue
 import com.oracle.bmc.auth.AuthenticationDetailsProvider
 import com.oracle.bmc.auth.ConfigFileAuthenticationDetailsProvider
 import com.oracle.bmc.queue.QueueClient
+import org.springframework.beans.factory.annotation.Qualifier
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.context.annotation.Profile
@@ -13,7 +14,7 @@ import java.io.IOException
 @Configuration
 class OciQueueConfig {
     @Bean
-    fun queueClient(provider: AuthenticationDetailsProvider): QueueClient {
+    fun queueClient(@Qualifier("authenticationDetailsProvider") provider: AuthenticationDetailsProvider): QueueClient {
         // Build the client and set the required messaging endpoint
         val client = QueueClient.builder().build(provider)
         client.setEndpoint("https://cell-1.queue.messaging.sa-saopaulo-1.oci.oraclecloud.com")
